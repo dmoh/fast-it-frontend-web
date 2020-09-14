cb088abc765e613c5f18e573dc8c2994d0a62a87
Elhade <elh-13@hotmail.fr>
2020-09-14 21:05:52 +0200

    Maj

---
+++ src/app/delivery/delivery-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AwaitingDeliveryComponent } from './awaiting-delivery/awaiting-delivery.component';
+import { DeliveryComponent } from './delivery.component';
+import { MyDeliveryComponent } from './my-delivery/my-delivery.component';
+import { ParameterComponent } from './parameter/parameter.component';
+
+
+const routes: Routes = [{
+  path: 'delivery', component: DeliveryComponent,
+  children: [
+    {
+      path: 'awaiting-delivery',
+      component: AwaitingDeliveryComponent,
+    },
+    {
+      path: 'my-delivery',
+      component: MyDeliveryComponent,
+    },
+    {
+      path: 'parameter',
+      component: ParameterComponent,
+    },
+  ]
+},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DeliveryRoutingModule { }
