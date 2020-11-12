20e007fb96dcf6e83e85a0b17bedc6f89f963cbb
Elhade <elh-13@hotmail.fr>
2020-11-13 00:39:58 +0100

    Finish Deliverer

--- src/app/delivery/delivery.component.ts
+++ src/app/delivery/delivery.component.ts
@@ -8,6 +8,8 @@ import {
 import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
 import { Router } from '@angular/router';
 import { User } from '@app/_models/user';
+import { AuthenticationService } from '@app/_services/authentication.service';
+import jwtDecode from 'jwt-decode';
 
 @Component({
   selector: 'app-delivery',
@@ -18,12 +20,14 @@ export class DeliveryComponent implements OnInit {
 
   orders: any;
   deliverer: User;
+  authorizedRoles: string[] = ["ROLE_SUPER_ADMIN","ROLE_DELIVERER"];
 
   constructor(private deliveryService: DeliveryService,
-    private snackBar: MatSnackBar,
     private router: Router,
+    private snackBar: MatSnackBar,
+    private authenticate: AuthenticationService,
     private bottomSheet: MatBottomSheet) { 
-      // this.router.navigate(['/delivery/awaiting-delivery']);
+      this.rolesBloqued();
     }
 
   ngOnInit(): void {
@@ -33,11 +37,24 @@ export class DeliveryComponent implements OnInit {
     } else {
       // Pas de support, proposer une alternative ?
     }
-    
-    this.deliveryService.getDeliverer().subscribe( deliverer => {
+  }
 
-    });
+  rolesBloqued() {
+    let currentUser: any = jwtDecode(this.authenticate.currentUserValue.token);
 
+    if (typeof currentUser != undefined && currentUser.roles && currentUser.roles.length > 0) {
+      let isAuthorized: boolean = false;
+      this.authorizedRoles.forEach( role => {
+        isAuthorized = currentUser.roles.includes(role.trim()) || isAuthorized;
+      });
+
+      if (!isAuthorized) {
+        console.log("Vous n'avez pas acces à cette page vous n'êtes pas un livreur.");
+        this.router.navigate(['home']);
+      }
+    } else {
+      this.router.navigate(['home']);
+    }
   }
   
 }
