41b6435479553ec487d7863d9d0b4dbe52611e47
Elhade <elh-13@hotmail.fr>
2022-03-07 23:02:19 +0100

    Changement des frais de services

--- src/app/cart/service/cart.service.ts
+++ src/app/cart/service/cart.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
-import {Product} from '../../models/product';
-import {Cart} from '../model/cart';
-import {BehaviorSubject, Observable} from 'rxjs/index';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {AuthenticationService} from '@app/_services/authentication.service';
-import {Router} from '@angular/router';
-import {environment} from '../../../environments/environment';
-import {isNumeric} from "tslint";
-import {RestaurantDashboardService} from "@app/restaurants/restaurant-dashboard/services/restaurant-dashboard.service";
-import {CartDetailComponent} from "@app/cart/cart-detail/cart-detail.component";
-import {limitDistanceSubscription} from "@app/_util/fasteat-constants";
-import {PromotionalCode} from "@app/_models/promotional-code";
-import {Promotion} from "@app/_models/promotion";
+import { Product } from '../../models/product';
+import { Cart } from '../model/cart';
+import { BehaviorSubject, Observable } from 'rxjs/index';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthenticationService } from '@app/_services/authentication.service';
+import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
+import { isNumeric } from "tslint";
+import { RestaurantDashboardService } from "@app/restaurants/restaurant-dashboard/services/restaurant-dashboard.service";
+import { CartDetailComponent } from "@app/cart/cart-detail/cart-detail.component";
+import { limitDistanceSubscription } from "@app/_util/fasteat-constants";
+import { PromotionalCode } from "@app/_models/promotional-code";
+import { Promotion } from "@app/_models/promotion";
 
 @Injectable({
   providedIn: 'root'
@@ -28,11 +28,11 @@ export class CartService {
   limitDistance = limitDistanceSubscription
 
   constructor(private http: HttpClient,
-              private authenticate: AuthenticationService,
-              private router: Router,
-              private restaurantDashboardService: RestaurantDashboardService
+    private authenticate: AuthenticationService,
+    private router: Router,
+    private restaurantDashboardService: RestaurantDashboardService
   ) {
-    this.headers = new HttpHeaders({'Content-Type': 'application/json; charset=utf-8'});
+    this.headers = new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' });
     if (localStorage.getItem('cart_fast_eat')) {
       this.setCart(JSON.parse(localStorage.getItem('cart_fast_eat')));
     }
@@ -40,18 +40,18 @@ export class CartService {
       // this.router.navigate(['/login']);
     }
     if (this.authenticate.tokenUserCurrent) {
-     this.headers.append(`Authorization: Bearer ${this.authenticate.tokenUserCurrent}`) ;
+      this.headers.append(`Authorization: Bearer ${this.authenticate.tokenUserCurrent}`);
     }
 
     this.authenticate.currentUser
-        .subscribe((user) => {
-          if (user) {
-            this.user = user;
-            if (user.data && user.data.subscription) {
-              this.cartCurrent.subscription = user.data.subscription;
-            }
+      .subscribe((user) => {
+        if (user) {
+          this.user = user;
+          if (user.data && user.data.subscription) {
+            this.cartCurrent.subscription = user.data.subscription;
           }
-        });
+        }
+      });
 
 
   }
@@ -59,35 +59,35 @@ export class CartService {
 
   UpdateCart(type: string, product?: Product, restaurant?: any): void { // todo enlever majuscule de cette méthode
     if (type === 'add') {
-        if (!this.cartCurrent) {
-            this.cartCurrent = new Cart();
-        }
-        if (this.cartCurrent.products.length === 0) {
-          product.indexProduct = 1;
-        } else {
-          product.indexProduct = this.cartCurrent.products.length++;
-        }
-        // let indexProd = 0;
-        if (this.cartCurrent.products.length > 0) {
-          this.cartCurrent.products = this.cartCurrent.products.filter((prod) => {
-            return typeof prod !== 'undefined' && prod !== null ? prod : '';
-          });
-        }
-        this.cartCurrent.products = [...this.cartCurrent.products, product];
-        this.cartCurrent.restaurant = restaurant;
-        this.generateTotalCart();
-        this.emitCartSubject();
+      if (!this.cartCurrent) {
+        this.cartCurrent = new Cart();
+      }
+      if (this.cartCurrent.products.length === 0) {
+        product.indexProduct = 1;
+      } else {
+        product.indexProduct = this.cartCurrent.products.length++;
+      }
+      // let indexProd = 0;
+      if (this.cartCurrent.products.length > 0) {
+        this.cartCurrent.products = this.cartCurrent.products.filter((prod) => {
+          return typeof prod !== 'undefined' && prod !== null ? prod : '';
+        });
+      }
+      this.cartCurrent.products = [...this.cartCurrent.products, product];
+      this.cartCurrent.restaurant = restaurant;
+      this.generateTotalCart();
+      this.emitCartSubject();
     } else if (type === 'update') {
-        const arrayProductsCurrent = this.cartCurrent.products;
-        const index = arrayProductsCurrent.findIndex(prod => prod.id === product.id);
-        this.cartCurrent.products[index] = product;
-        this.generateTotalCart();
-        this.emitCartSubject();
+      const arrayProductsCurrent = this.cartCurrent.products;
+      const index = arrayProductsCurrent.findIndex(prod => prod.id === product.id);
+      this.cartCurrent.products[index] = product;
+      this.generateTotalCart(true);
+      this.emitCartSubject();
     } else if (type === 'remove') {
-        this.cartCurrent.products = this.cartCurrent.products.filter((prod: Product) => product.id !== prod.id);
-        this.generateTotalCart();
-        this.emitCartSubject();
-    }else if (type === 'empty-cart') {
+      this.cartCurrent.products = this.cartCurrent.products.filter((prod: Product) => product.id !== prod.id);
+      this.generateTotalCart(true);
+      this.emitCartSubject();
+    } else if (type === 'empty-cart') {
       this.cartCurrent = new Cart();
       this.emitCartSubject('empty');
     }
@@ -107,7 +107,7 @@ export class CartService {
     this.emitCartSubject();
   }
 
-  emitCartSubject(empty?: string){
+  emitCartSubject(empty?: string) {
     if (empty === 'empty') {
       localStorage.removeItem('cart_fast_eat');
       this.cartCurrent = new Cart();
@@ -141,10 +141,10 @@ export class CartService {
         this.cartCurrent.totalAmountProduct += +(prod.quantity * prod.amount) / 100;
         this.cartCurrent.amountWithoutSpecialOffer += +(prod.quantity * prod.amount) / 100;
 
-        if((this.cartCurrent.promotionalCode
-            && this.cartCurrent.promotionalCode.percentage
-            && +this.cartCurrent.promotionalCode.percentage > 0)
-            || this.hasPercentPromotionSubscription()
+        if ((this.cartCurrent.promotionalCode
+          && this.cartCurrent.promotionalCode.percentage
+          && +this.cartCurrent.promotionalCode.percentage > 0)
+          || this.hasPercentPromotionSubscription()
         ) {
           if (this.hasPercentPromotionSubscription()) {
             this.cartCurrent.promotionalCode = new Promotion();
@@ -155,32 +155,32 @@ export class CartService {
     });
 
     if ((this.cartCurrent.promotionalCode
-        && this.cartCurrent.promotionalCode.percentage
-        && +this.cartCurrent.promotionalCode.percentage > 0)
-        || this.hasPercentPromotionSubscription()
-    ){
-      if(this.hasPercentPromotionSubscription()) {
+      && this.cartCurrent.promotionalCode.percentage
+      && +this.cartCurrent.promotionalCode.percentage > 0)
+      || this.hasPercentPromotionSubscription()
+    ) {
+      if (this.hasPercentPromotionSubscription()) {
         this.cartCurrent.promotionalCode.percentage = this.user.data.subscription.percent;
       }
       this.cartCurrent.promotionalCode.totalAmountProductWithPromotion = 0;
       this.cartCurrent.promotionalCode.totalAmountProductWithPromotion
-          = this.cartCurrent.promotionalCode.totalAmountProduct
-          - (this.cartCurrent.promotionalCode.percentage/100*this.cartCurrent.promotionalCode.totalAmountProduct)
-      ;
-      this.cartCurrent.total = this.cartCurrent.total - (this.cartCurrent.promotionalCode.percentage/100*this.cartCurrent.total);
+        = this.cartCurrent.promotionalCode.totalAmountProduct
+        - (this.cartCurrent.promotionalCode.percentage / 100 * this.cartCurrent.promotionalCode.totalAmountProduct)
+        ;
+      this.cartCurrent.total = this.cartCurrent.total - (this.cartCurrent.promotionalCode.percentage / 100 * this.cartCurrent.total);
       this.cartCurrent.totalAmountProduct = this.cartCurrent.promotionalCode.totalAmountProductWithPromotion;
       this.cartCurrent.amountWithoutSpecialOffer = this.cartCurrent.promotionalCode.totalAmountProductWithPromotion;
     }
 
     // check if merchant as already promotion
     if (
-        typeof this.cartCurrent.restaurant.specialOffer !== 'undefined'
-        && !this.hasPercentPromotionSubscription()
+      typeof this.cartCurrent.restaurant.specialOffer !== 'undefined'
+      && !this.hasPercentPromotionSubscription()
     ) {
       if (
         +(this.cartCurrent.restaurant.specialOffer.minimumAmountForOffer) <=
         +(this.cartCurrent.total)
-      ){
+      ) {
         this.cartCurrent.total -= +(this.cartCurrent.restaurant.specialOffer.specialOfferAmount);
       }
     }
@@ -199,7 +199,10 @@ export class CartService {
     this.cartCurrent.amountWithoutSpecialOffer += this.isFreeShippingCost() ? 0 : +(this.cartCurrent.deliveryCost);
 
     if (isCheckout) {
-      this.cartCurrent.stripeFee = (this.cartCurrent.total - (this.cartCurrent.total * 0.986)) + 2.85 ;
+      alert(this.cartCurrent.totalAmountProduct);
+      const fastItFee = this.getFastItFee( this.cartCurrent?.promotionalCode?.totalAmountProduct ?? this.cartCurrent.totalAmountProduct);
+      alert(fastItFee)
+      this.cartCurrent.stripeFee = (this.cartCurrent.total - (this.cartCurrent.total * 0.986)) + 0.35 + 2.50 - fastItFee;
       const fee = (this.cartCurrent.stripeFee).toFixed(2);
       this.cartCurrent.total += parseFloat(fee);
       this.cartCurrent.totalWithoutDiscount += parseFloat(fee);
@@ -241,20 +244,20 @@ export class CartService {
 
 
   getFormTokenPaymentSystemPay(
-      amountCart: number,
-      restId: number,
-      delivery: number,
-      stripe?: number,
-      currencyCart: string = 'EUR'): Observable<any> {
+    amountCart: number,
+    restId: number,
+    delivery: number,
+    stripe?: number,
+    currencyCart: string = 'EUR'): Observable<any> {
 
     return this.http.post<any>(`${this.urlApi}payment/token-payment`,
-        {
-          amount: amountCart,
-          currency: currencyCart,
-          restaurantId: restId,
-          deliveryCost: delivery,
-          stripeFee: stripe
-        }, this.headers);
+      {
+        amount: amountCart,
+        currency: currencyCart,
+        restaurantId: restId,
+        deliveryCost: delivery,
+        stripeFee: stripe
+      }, this.headers);
   }
 
 
@@ -265,12 +268,12 @@ export class CartService {
 
   // saveOrder()
   saveOrder(cartOrder: {}): Observable<any> {
-    return this.http.post<any>(`${ this.urlApi }order/save`,
+    return this.http.post<any>(`${this.urlApi}order/save`,
       JSON.stringify(cartOrder), this.headers);
   }
 
   getCostDelivery(dataDistance: any): Observable<any> {
-    return this.http.post<any>(`${ this.urlApi }delivery/cost`,
+    return this.http.post<any>(`${this.urlApi}delivery/cost`,
       JSON.stringify(dataDistance), this.headers);
   }
   saveCodeCustomerToDeliver(responseCustomer): Observable<any> {
@@ -329,5 +332,19 @@ export class CartService {
     this.cartCurrent.distance = distance;
     this.generateTotalCart();
     this.emitCartSubject();
+  }
+
+  getFastItFee(totalCount: number) {
+    if (totalCount <= 7) {
+      return 0.50;
+    } else if (totalCount <= 10) {
+      return 1.00;
+    } else if (totalCount <= 15) {
+      return 1.50;
+    } else if (totalCount <= 20) {
+      return 2.00;
+    } else if (totalCount > 20) {
+      return 2.50;
+    }
   }
 }
